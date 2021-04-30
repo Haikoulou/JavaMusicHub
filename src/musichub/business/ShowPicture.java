e6fd0efdfd8cf5cc7c3fcb3ea399d44428178540
AnisFbr <81908727+AnisFbr@users.noreply.github.com>
2021-04-30 14:37:24 +0200

    Test2

---
+++ src/musichub/business/ShowPicture.java
@@ -0,0 +1,15 @@
+package musichub.business;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+
+public class ShowPicture {
+/*	JFrame frame = new JFrame();
+	ImageIcon icon = new ImageIcon("file.png");
+	JLabel label= new JLabel(icon);
+	frame.add(label);
+	frame.setDefaultCloseOperation(JFrame.EXIT_ON_ClOSE);
+	frame.pack();
+	frame.setVisible(true);*/
+}
+
+//test
