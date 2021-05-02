a240aa07e7bca56cad20321478d1e4622d7602a0
AnisFbr <81908727+AnisFbr@users.noreply.github.com>
2021-05-02 13:43:51 +0200

    added javadoc

--- src/musichub/business/NoAlbumFoundException.java
+++ src/musichub/business/NoAlbumFoundException.java
@@ -1,7 +1,15 @@
 package musichub.business;
 
+
 public class NoAlbumFoundException extends LogException {
 
+	/**
+	 * <b>NoAlbumFoundException constructor<b/>
+	 * 
+	 * this constructor creates an object msg 
+	 * 
+	 * @param msg: send a warning to the client if the album doesn't exist.
+	 */
 	public NoAlbumFoundException (String msg) {
 		super("WARNING", msg);
 	}
