9a80319f14cb184b97adef561b9a26dd70cd80b3
AnisFbr <81908727+AnisFbr@users.noreply.github.com>
2021-05-02 13:47:34 +0200

    added javadoc

--- src/musichub/business/NoElementFoundException.java
+++ src/musichub/business/NoElementFoundException.java
@@ -3,6 +3,13 @@ package musichub.business;
 import java.lang.Exception;
 
 public class NoElementFoundException extends LogException {
+	/**
+	 * <b>NoElementFoundException constructor<b/>
+	 * 
+	 * this constructor creates an object msg 
+	 * 
+	 * @param msg: send a warning to the client if the element doesn't exist.
+	 */
 
 	public NoElementFoundException (String msg) {
 		super("WARNING", msg);
